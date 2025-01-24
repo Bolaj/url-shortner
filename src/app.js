f6d5fde46b1b2bb8913c63dc88fc4fae8aae23fe
TimiBolaji <mattybolaji@gmail.com>
2025-01-24 23:07:58 +0100

    --allow cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import express from "express"
 import bodyParser from "body-parser"
 import connectDB from "./config/db.js"
 import urlRoutes from "./routes/urlRoutes.js"
+import cors from "cors";
+
 
 const app = express()
 const port = 3000
 
 connectDB();
 
+const corsOptions = {
+  origin: ["http://localhost:3001"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json())
 
