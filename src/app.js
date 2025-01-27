333ecb23a0e9437ce7b3abb9da1acf41a7bff12c
TimiBolaji <mattybolaji@gmail.com>
2025-01-27 20:31:49 +0100

    fix cors

--- src/app.js
+++ src/app.js
@@ -3,20 +3,23 @@ import bodyParser from "body-parser"
 import connectDB from "./config/db.js"
 import urlRoutes from "./routes/urlRoutes.js"
 import cors from "cors";
-
+import dotenv from "dotenv";
 
 const app = express()
 const port = 3000
+dotenv.config();
 
 connectDB();
 
-const corsOptions = {
-  origin: ["http://localhost:3001"], 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+const frontendUrl = process.env.FRONTEND_URL;
+console.log(`Frontend URL: ${frontendUrl}`);
 
-app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    origin: frontendUrl,
+  })
+);
 
 app.use(bodyParser.json())
 
